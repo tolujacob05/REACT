fbe39ee0ee8a02c12aac93f88e3a6fd4a53bcc4f
tolujacob05 <tolujacob05@gmail.com>
2023-09-16 12:06:38 +0100

    done

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
-import App from "./App-v2";
+// import App from "./App-v2";
+import App from "./App-v3";
 
 /*function Test() {
   const [movieRating, setMovieRating] = useState(0);
