f110b030b1199264a6785e3bc61cdf33e2693257
tolujacob05 <tolujacob05@gmail.com>
2023-10-26 10:57:46 +0100

    done

--- react-pizza/src/features/order/OrderItem.jsx
+++ react-pizza/src/features/order/OrderItem.jsx
@@ -3,16 +3,17 @@ import { formatCurrency } from "../../utilities/helpers";
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
-  console.log(isLoadingIngredients, ingredients);
-
   return (
-    <li className="py-3">
+    <li className="py-3 space-y-1">
       <div className="flex items-center justify-between gap-4 text-sm">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm italic uppercase text-stone-500">
+        {isLoadingIngredients ? "Loading..." : ingredients.join(", ")}
+      </p>
     </li>
   );
 }
